505c666c2734b94021ed345e0856c256545854a5
Christian <christian.poma.n@uni.pe>
2024-06-25 19:36:59 -0500

    user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,56 @@
 import User from "../models/User.js";
 
 const registrar = async (req, res) => {
-	const user = req.body;
-	console.log(user);
-	const isRegistered = await User.findOne({ username: user.username});
-	console.log(isRegistered);
-	if (isRegistered) {
-		const error = new Error("User is signed up already");
-		return res.status(400).json({ msg: error.message });
-	}
+	console.log("-----------------------");
+	console.log(req.body);
+	const { username, password, email, fullName } = req.body;
 
 	try {
-		const user = new User(req.body);
+		const existingUser = await User.findOne({ username });
+		if (existingUser) {
+			return res.status(400).json({ msg: "El usuario ya está registrado." });
+		}
+
+		const user = new User({
+			fullName,
+			username,
+			email,
+			password
+		});
+
 		await user.save();
 		res.status(201).json({
-			msg: "User created successfully. Please check your email to confirm your account.",
+			msg: "Usuario creado exitosamente. Por favor revisa tu email para confirmar tu cuenta.",
 		});
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ msg: "Server error" });
+		res.status(500).json({ msg: "Error del servidor" });
 	}
 };
 
 const autenticar = async (req, res) => {
-	const { username, password } = req.body;
+	const { email, password } = req.body;
 
-	try{
-		// Comprobar si el user existe
-		const user = await User.findOne({ username });
+	try {
+		const user = await User.findOne({ email: email });
+		console.log(user);
 		if (!user) {
-			console.log("user no existe");
-			const error = new Error("El user no existe");
-			return res.status(404).json({ msg: error.message });
+			return res.status(404).json({ msg: "El usuario no existe" });
 		}
 
-		if (user.password != password) {
-			const error = new Error("El password es incorrecto");
-				return res.status(401).json({ msg: error.message });
+		const isMatch = (password === user.password)
+		if (!isMatch) {
+			return res.status(401).json({ msg: "Contraseña incorrecta" });
+		}
 
-		} 
 		res.status(200).json({
 			msg: "Usuario autenticado correctamente",
-			username: user.username,
+			user: user,
 		});
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ msg: "Server error" });
+		res.status(500).json({ msg: "Error del servidor" });
 	}
-
-
-	
-
 };
 
 export {
--- models/User.js
+++ models/User.js
@@ -2,17 +2,23 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
     username: {
       type: String,
       required: true,
       trim: true,
     },
-    // email: {
-    //   type: String,
-    //   required: true,
-    //   trim: true,
-    //   unique: true,
-    // },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
     password: {
       type: String,
       required: true,
